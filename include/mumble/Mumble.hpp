mumble-voip/libmumble

commit b8f0fc40973b6ecb7818631db2f2fee888f1667e

Enable pedantic warnings and fix resulting errors

src/Cert.cpp
@@ -262,15 +262,15 @@ Attributes P::parseX509Name(const X509_NAME *name) {
 	return attributes;
 }
 
-int P::passwordCallback(char *buf, int size, int, void *userdata) {
-	const auto password = *static_cast< std::string_view * >(userdata);
+int P::passwordCallback(char *buf, const int32_t size, int, void *userdata) {
+	auto password = static_cast< const std::string_view * >(userdata);
 
-	auto length = password.size();
-	if (length > size) {
+	auto length = password->size();
+	if (length > static_cast< uint32_t >(size)) {
 		length = size;
 	}
 
-	memcpy(buf, password.data(), length);
+	memcpy(buf, password->data(), length);
 
 	return length;
 }

src/Cert.hpp
@@ -27,7 +27,7 @@ private:
 	static std::string parseASN1String(const ASN1_STRING *string);
 	static Attributes parseX509Name(const X509_NAME *name);
 
-	static int passwordCallback(char *buf, int size, int, void *userdata);
+	static int passwordCallback(char *buf, const int32_t size, int, void *userdata);
 
 	X509 *m_x509;
 };

src/Key.cpp
@@ -129,15 +129,15 @@ P::~P() {
 	}
 }
 
-int P::passwordCallback(char *buf, int size, int, void *userdata) {
-	const auto password = *static_cast< std::string_view * >(userdata);
+int P::passwordCallback(char *buf, const int32_t size, int, void *userdata) {
+	auto password = static_cast< const std::string_view * >(userdata);
 
-	auto length = password.size();
-	if (length > size) {
+	auto length = password->size();
+	if (length > static_cast< uint32_t >(size)) {
 		length = size;
 	}
 
-	memcpy(buf, password.data(), length);
+	memcpy(buf, password->data(), length);
 
 	return length;
 }

src/Key.hpp
@@ -22,7 +22,7 @@ public:
 	~P();
 
 private:
-	static int passwordCallback(char *buf, int size, int, void *userdata);
+	static int passwordCallback(char *buf, const int32_t size, int, void *userdata);
 
 	EVP_PKEY *m_pkey;
 };

src/Monitor.cpp
@@ -126,7 +126,9 @@ bool Monitor::trigger() {
 #else
 	constexpr uint8_t byte = 0;
 #endif
-	return send(m_trigger.second.fd(), &byte, sizeof(byte), 0) >= sizeof(byte);
+	static_assert(sizeof(byte) == 1);
+
+	return send(m_trigger.second.fd(), &byte, sizeof(byte), 0) >= 1;
 }
 
 bool Monitor::untrigger() {
@@ -138,7 +140,9 @@ bool Monitor::untrigger() {
 #else
 	uint8_t byte;
 #endif
-	return recv(m_trigger.first.fd(), &byte, sizeof(byte), 0) >= sizeof(byte);
+	static_assert(sizeof(byte) == 1);
+
+	return recv(m_trigger.first.fd(), &byte, sizeof(byte), 0) >= 1;
 }
 
 uint32_t Monitor::wait(const EventsRef events, const uint32_t timeout) {

src/Opus.cpp
@@ -185,11 +185,13 @@ EXPORT uint32_t Encoder::bitrate() const {
 }
 
 EXPORT bool Encoder::setBitrate(const uint32_t bitrate) {
-	opus_int32 value = bitrate;
-	if (value == infinite32) {
+	opus_int32 value;
+	if (bitrate == infinite32) {
 		value = OPUS_BITRATE_MAX;
-	} else if (!value) {
+	} else if (!bitrate) {
 		value = OPUS_AUTO;
+	} else {
+		value = bitrate;
 	}
 
 	return m_p->set(OPUS_SET_BITRATE(value));

src/Socket.cpp
@@ -64,7 +64,7 @@ Socket::operator bool() const {
 	return m_fd != invalidFD;
 }
 
-const int32_t Socket::fd() const {
+int32_t Socket::fd() const {
 	return m_fd;
 }
 

src/TCP.cpp
@@ -40,7 +40,8 @@ std::pair< int, int32_t > SocketTCP::accept(Endpoint &endpoint) {
 #else
 	socklen_t size = sizeof(addr);
 #endif
-	const auto fd = ::accept(m_fd, reinterpret_cast< sockaddr * >(&addr), &size);
+	// Explicit return data type because socket handles are unsigned on Windows.
+	const int8_t fd = ::accept(m_fd, reinterpret_cast< sockaddr * >(&addr), &size);
 	if (fd == invalidFD) {
 		return { osError(), invalidFD };
 	}

commit 0aacf26783a0b60a8f9dd7308a6c11a32f678ca2

Fix implicit cast warnings
This also fixes a bug introduced in b8f0fc40973b6ecb7818631db2f2fee888f1667e.

A 8 bit integer was being used in SocketTCP::accept() instead of a 32 bit one.

include/mumble/Mumble.hpp
@@ -21,8 +21,8 @@ public:
 
 		uint32_t blob() const { return major << 16 | minor << 8 | patch; }
 
-		Version(const uint32_t blob) : major(blob >> 16), minor(blob >> 8), patch(blob) {}
-		Version(const uint16_t major, const uint16_t minor, const uint8_t patch)
+		Version(const uint32_t blob) : major(blob >> 16), minor(static_cast< uint8_t >(blob >> 8)), patch(static_cast< uint8_t >(blob)) {}
+		Version(const uint16_t major, const uint8_t minor, const uint8_t patch)
 			: major(major), minor(minor), patch(patch) {}
 	};
 

src/Base64.cpp
@@ -20,6 +20,7 @@
 
 #define CAST_BUF(var) (reinterpret_cast< unsigned char * >(var))
 #define CAST_BUF_CONST(var) (reinterpret_cast< const unsigned char * >(var))
+#define CAST_SIZE(var) (static_cast< int >(var))
 
 using namespace mumble;
 
@@ -52,7 +53,7 @@ size_t Base64::decode(const BufRef out, const BufRefConst in) {
 	EVP_DecodeInit(m_p->m_ctx);
 
 	int written_1;
-	if (EVP_DecodeUpdate(m_p->m_ctx, CAST_BUF(out.data()), &written_1, CAST_BUF_CONST(in.data()), in.size()) < 0) {
+	if (EVP_DecodeUpdate(m_p->m_ctx, CAST_BUF(out.data()), &written_1, CAST_BUF_CONST(in.data()), CAST_SIZE(in.size())) < 0) {
 		return {};
 	}
 
@@ -70,11 +71,11 @@ size_t Base64::encode(const BufRef out, const BufRefConst in) {
 		// +1 for the NUL terminator.
 		//
 		// EVP_EncodeBlock() adds padding when the input is not divisible by 3.
-		return ceilf(in.size() / 3.f) * 4 + 1;
+		return static_cast< size_t >(ceilf(in.size() / 3.f) * 4 + 1);
 	}
 
 	// EVP_EncodeBlock() returns the length of the string without the NUL terminator.
-	const auto written = EVP_EncodeBlock(CAST_BUF(out.data()), CAST_BUF_CONST(in.data()), in.size());
+	const auto written = EVP_EncodeBlock(CAST_BUF(out.data()), CAST_BUF_CONST(in.data()), CAST_SIZE(in.size()));
 	if (written < 0) {
 		return {};
 	}

src/Cert.cpp
@@ -30,6 +30,8 @@
 		return {}; \
 	}
 
+#define CAST_SIZE(var) (static_cast< int >(var))
+
 using namespace mumble;
 
 using Attributes = Cert::Attributes;
@@ -190,11 +192,11 @@ P::P(X509 *x509) : m_x509(x509) {
 
 P::P(const DerRefConst der) {
 	auto bytes = boost::as_bytes(der);
-	d2i_X509(&m_x509, reinterpret_cast< const unsigned char ** >(&bytes), bytes.size());
+	d2i_X509(&m_x509, reinterpret_cast< const unsigned char ** >(&bytes), static_cast< long >(bytes.size()));
 }
 
 P::P(const std::string_view pem, std::string_view password) : m_x509(nullptr) {
-	auto bio = BIO_new_mem_buf(pem.data(), pem.size());
+	auto bio = BIO_new_mem_buf(pem.data(), CAST_SIZE(pem.size()));
 	if (!bio) {
 		return;
 	}
@@ -262,11 +264,11 @@ Attributes P::parseX509Name(const X509_NAME *name) {
 	return attributes;
 }
 
-int P::passwordCallback(char *buf, const int32_t size, int, void *userdata) {
+int P::passwordCallback(char *buf, const int size, int, void *userdata) {
 	auto password = static_cast< const std::string_view * >(userdata);
 
-	auto length = password->size();
-	if (length > static_cast< uint32_t >(size)) {
+	auto length = CAST_SIZE(password->size());
+	if (length > size) {
 		length = size;
 	}
 

src/Cert.hpp
@@ -27,7 +27,7 @@ private:
 	static std::string parseASN1String(const ASN1_STRING *string);
 	static Attributes parseX509Name(const X509_NAME *name);
 
-	static int passwordCallback(char *buf, const int32_t size, int, void *userdata);
+	static int passwordCallback(char *buf, const int size, int, void *userdata);
 
 	X509 *m_x509;
 };

src/Crypt.cpp
@@ -19,6 +19,7 @@
 
 #define CAST_BUF(var) (reinterpret_cast< unsigned char * >(var))
 #define CAST_BUF_CONST(var) (reinterpret_cast< const unsigned char * >(var))
+#define CAST_SIZE(var) (static_cast< int >(var))
 
 using namespace mumble;
 
@@ -68,13 +69,13 @@ uint32_t Crypt::blockSize() const {
 uint32_t Crypt::keySize() const {
 	CHECK
 
-	return m_p->m_key.size();
+	return static_cast< uint32_t >(m_p->m_key.size());
 }
 
 uint32_t Crypt::nonceSize() const {
 	CHECK
 
-	return m_p->m_nonce.size();
+	return static_cast< uint32_t >(m_p->m_nonce.size());
 }
 
 BufRefConst Crypt::key() const {
@@ -101,7 +102,7 @@ bool Crypt::setKey(const BufRefConst key) {
 	CHECK
 
 	if (m_p->m_key.size() != key.size()) {
-		if (EVP_CIPHER_CTX_set_key_length(m_p->m_ctx, key.size()) <= 0) {
+		if (EVP_CIPHER_CTX_set_key_length(m_p->m_ctx, CAST_SIZE(key.size())) <= 0) {
 			return false;
 		}
 
@@ -126,7 +127,7 @@ Buf Crypt::genNonce() const {
 	}
 
 	Buf nonce(size);
-	if (RAND_priv_bytes(CAST_BUF(nonce.data()), nonce.size()) <= 0) {
+	if (RAND_priv_bytes(CAST_BUF(nonce.data()), CAST_SIZE(nonce.size())) <= 0) {
 		return {};
 	}
 
@@ -137,7 +138,7 @@ bool Crypt::setNonce(const BufRefConst nonce) {
 	CHECK
 
 	if (m_p->m_nonce.size() != nonce.size()) {
-		if (EVP_CIPHER_CTX_ctrl(m_p->m_ctx, EVP_CTRL_AEAD_SET_IVLEN, nonce.size(), nullptr) <= 0) {
+		if (EVP_CIPHER_CTX_ctrl(m_p->m_ctx, EVP_CTRL_AEAD_SET_IVLEN, CAST_SIZE(nonce.size()), nullptr) <= 0) {
 			return false;
 		}
 
@@ -257,7 +258,7 @@ size_t P::process(const bool encrypt, const BufRef out, const BufRefConst in, co
 	}
 
 	if (!encrypt && !tag.empty()) {
-		if (EVP_CIPHER_CTX_ctrl(m_ctx, EVP_CTRL_AEAD_SET_TAG, tag.size(), tag.data()) <= 0) {
+		if (EVP_CIPHER_CTX_ctrl(m_ctx, EVP_CTRL_AEAD_SET_TAG, CAST_SIZE(tag.size()), tag.data()) <= 0) {
 			return {};
 		}
 	}
@@ -265,12 +266,15 @@ size_t P::process(const bool encrypt, const BufRef out, const BufRefConst in, co
 	int written1;
 
 	if (!aad.empty()) {
-		if (EVP_CipherUpdate(m_ctx, nullptr, &written1, CAST_BUF_CONST(aad.data()), aad.size()) <= 0) {
+		if (EVP_CipherUpdate(m_ctx, nullptr, &written1, CAST_BUF_CONST(aad.data()), CAST_SIZE(aad.size()))
+			<= 0) {
 			return {};
 		}
 	}
 
-	if (EVP_CipherUpdate(m_ctx, CAST_BUF(out.data()), &written1, CAST_BUF_CONST(in.data()), in.size()) <= 0) {
+	if (EVP_CipherUpdate(m_ctx, CAST_BUF(out.data()), &written1, CAST_BUF_CONST(in.data()),
+						 CAST_SIZE(in.size()))
+		<= 0) {
 		return {};
 	}
 
@@ -281,7 +285,7 @@ size_t P::process(const bool encrypt, const BufRef out, const BufRefConst in, co
 	}
 
 	if (encrypt && !tag.empty()) {
-		if (EVP_CIPHER_CTX_ctrl(m_ctx, EVP_CTRL_AEAD_GET_TAG, tag.size(), tag.data()) <= 0) {
+		if (EVP_CIPHER_CTX_ctrl(m_ctx, EVP_CTRL_AEAD_GET_TAG, CAST_SIZE(tag.size()), tag.data()) <= 0) {
 			return {};
 		}
 	}

src/CryptOCB2.cpp
@@ -22,6 +22,7 @@
 
 #define CAST_BUF(var) (reinterpret_cast< unsigned char * >(var))
 #define CAST_BUF_CONST(var) (reinterpret_cast< const unsigned char * >(var))
+#define CAST_SIZE(var) (static_cast< int >(var))
 
 using namespace mumble;
 
@@ -98,7 +99,7 @@ Buf CryptOCB2::genNonce() const {
 	CHECK
 
 	Buf nonce(P::nonceSize);
-	if (RAND_priv_bytes(reinterpret_cast< unsigned char * >(nonce.data()), nonce.size()) <= 0) {
+	if (RAND_priv_bytes(CAST_BUF(nonce.data()), CAST_SIZE(nonce.size())) <= 0) {
 		return {};
 	}
 
@@ -345,7 +346,7 @@ size_t P::process(const bool encrypt, const BufRef out, const BufRefConst in) {
 
 	int written1;
 
-	if (EVP_CipherUpdate(m_ctx, CAST_BUF(out.data()), &written1, CAST_BUF_CONST(in.data()), in.size()) <= 0) {
+	if (EVP_CipherUpdate(m_ctx, CAST_BUF(out.data()), &written1, CAST_BUF_CONST(in.data()), CAST_SIZE(in.size())) <= 0) {
 		return {};
 	}
 

src/Key.cpp
@@ -21,6 +21,8 @@
 		return {}; \
 	}
 
+#define CAST_SIZE(var) (static_cast< int >(var))
+
 using namespace mumble;
 
 using P = Key::P;
@@ -116,7 +118,7 @@ P::P(EVP_PKEY *pkey) : m_pkey(pkey) {
 }
 
 P::P(const std::string_view pem, const bool isPrivate, std::string_view password) : m_pkey(nullptr) {
-	auto bio = BIO_new_mem_buf(pem.data(), pem.size());
+	auto bio = BIO_new_mem_buf(pem.data(), CAST_SIZE(pem.size()));
 	if (!bio) {
 		return;
 	}
@@ -136,11 +138,11 @@ P::~P() {
 	}
 }
 
-int P::passwordCallback(char *buf, const int32_t size, int, void *userdata) {
+int P::passwordCallback(char *buf, const int size, int, void *userdata) {
 	auto password = static_cast< const std::string_view * >(userdata);
 
-	auto length = password->size();
-	if (length > static_cast< uint32_t >(size)) {
+	auto length = CAST_SIZE(password->size());
+	if (length > size) {
 		length = size;
 	}
 

src/Key.hpp
@@ -22,7 +22,7 @@ public:
 	~P();
 
 private:
-	static int passwordCallback(char *buf, const int32_t size, int, void *userdata);
+	static int passwordCallback(char *buf, const int size, int, void *userdata);
 
 	EVP_PKEY *m_pkey;
 };

src/Monitor.cpp
@@ -59,7 +59,7 @@ Monitor::operator bool() const {
 }
 
 uint32_t Monitor::num() const {
-	return m_fds.size();
+	return static_cast< uint32_t >(m_fds.size());
 }
 
 bool Monitor::add(const int32_t fd, const bool in, const bool out) {
@@ -160,7 +160,7 @@ uint32_t Monitor::wait(const EventsRef events, const uint32_t timeout) {
 #	include <boost/core/span.hpp>
 
 uint32_t Monitor::waitEpoll(const EventsRef events, const uint32_t timeout) {
-	const int32_t ret = epoll_wait(m_handle, m_targets.data(), m_targets.size(), timeout == infinite32 ? -1 : timeout);
+	const int32_t ret = epoll_wait(m_handle, m_targets.data(), static_cast< int >(m_targets.size()), timeout == infinite32 ? -1 : timeout);
 	if (ret < 1) {
 		return {};
 	}
@@ -212,7 +212,7 @@ uint32_t Monitor::waitPoll(const EventsRef events, const uint32_t timeout) {
 	uint32_t num = 0;
 
 	for (const auto &target : m_targets) {
-		if (num >= events.size() || num >= ret) {
+		if (num >= events.size() || num >= static_cast< uint32_t >(ret)) {
 			break;
 		}
 

src/Opus.cpp
@@ -18,6 +18,12 @@
 		return {}; \
 	}
 
+#define CAST_BUF(var) (reinterpret_cast< unsigned char * >(var))
+#define CAST_BUF_CONST(var) (reinterpret_cast< const unsigned char * >(var))
+#define CAST_FPTR(var) (reinterpret_cast< float * >(var))
+#define CAST_FPTR_CONST(var) (reinterpret_cast< const float * >(var))
+#define CAST_SIZE(var) (static_cast< opus_int32 >(var))
+
 using namespace mumble;
 
 using Decoder = mumble::OpusDecoder;
@@ -58,10 +64,10 @@ Decoder::operator bool() const {
 }
 
 size_t Decoder::operator()(const BufRef out, const BufRefConst in, const bool decodeFEC) {
-	const auto samples = out.size() / sizeof(float) / m_p->m_channels;
+	const auto samples = static_cast< int >(out.size() / sizeof(float) / m_p->m_channels);
 
-	const auto written = opus_decode_float(m_p->m_ctx.get(), reinterpret_cast< const unsigned char * >(in.data()),
-										   in.size(), reinterpret_cast< float * >(out.data()), samples, decodeFEC);
+	const auto written = opus_decode_float(m_p->m_ctx.get(), CAST_BUF_CONST(in.data()),
+										   CAST_SIZE(in.size()), CAST_FPTR(out.data()), samples, decodeFEC);
 
 	return written >= 0 ? written : 0;
 }
@@ -105,7 +111,7 @@ bool Decoder::togglePhaseInversion(const bool enable) {
 
 uint32_t Decoder::packetSamples(const BufRefConst packet) {
 	const auto ret = opus_decoder_get_nb_samples(
-		m_p->m_ctx.get(), reinterpret_cast< const unsigned char * >(packet.data()), packet.size());
+		m_p->m_ctx.get(), CAST_BUF_CONST(packet.data()), CAST_SIZE(packet.size()));
 	return ret >= 0 ? ret : 0;
 }
 
@@ -125,10 +131,10 @@ Encoder::operator bool() const {
 }
 
 size_t Encoder::operator()(const BufRef out, const BufRefConst in) {
-	const auto samples = in.size() / sizeof(float) / m_p->m_channels;
+	const auto samples = static_cast< int >(in.size() / sizeof(float) / m_p->m_channels);
 
-	const auto written = opus_encode_float(m_p->m_ctx.get(), reinterpret_cast< const float * >(in.data()), samples,
-										   reinterpret_cast< unsigned char * >(out.data()), out.size());
+	const auto written = opus_encode_float(m_p->m_ctx.get(), CAST_FPTR_CONST(in.data()), samples,
+										   CAST_BUF(out.data()), CAST_SIZE(out.size()));
 
 	return written >= 0 ? written : 0;
 }

src/Pack.cpp
@@ -48,17 +48,19 @@ UDP::Pack(const uint32_t dataSize, const NetHeader &header) : mumble::Pack< NetH
 	memcpy(m_buf.data(), &header, sizeof(header));
 }
 
-TCP::Pack(const google::protobuf::Message &proto) : mumble::Pack< NetHeader >(proto.ByteSizeLong()) {
+TCP::Pack(const google::protobuf::Message &proto)
+	: mumble::Pack< NetHeader >(static_cast< uint32_t >(proto.ByteSizeLong())) {
 	auto buf = data();
-	if (proto.SerializeToArray(buf.data(), buf.size())) {
+	if (proto.SerializeToArray(buf.data(), static_cast< int >(buf.size()))) {
 		header().type = Endian::toNetwork(static_cast< uint16_t >(proto.GetDescriptor()->index()));
 		header().size = Endian::toNetwork(static_cast< uint32_t >(buf.size()));
 	}
 }
 
-UDP::Pack(const google::protobuf::Message &proto) : mumble::Pack< NetHeader >(proto.ByteSizeLong()) {
+UDP::Pack(const google::protobuf::Message &proto)
+	: mumble::Pack< NetHeader >(static_cast< uint32_t >(proto.ByteSizeLong())) {
 	auto buf = data();
-	if (proto.SerializeToArray(buf.data(), buf.size())) {
+	if (proto.SerializeToArray(buf.data(), static_cast< int >(buf.size()))) {
 		header().type = static_cast< uint8_t >(proto.GetDescriptor()->index());
 	}
 }
@@ -614,7 +616,7 @@ bool TCP::operator()(Message &message, uint32_t dataSize) const {
 	}
 
 	if (dataSize > data().size()) {
-		dataSize = data().size();
+		dataSize = static_cast< decltype(dataSize) >(data().size());
 	}
 
 	switch (message.type()) {
@@ -1116,7 +1118,7 @@ bool UDP::operator()(Message &message, uint32_t dataSize) const {
 	}
 
 	if (dataSize > data().size()) {
-		dataSize = data().size();
+		dataSize = static_cast< decltype(dataSize) >(data().size());
 	}
 
 	switch (message.type()) {

src/Peer.cpp
@@ -336,7 +336,7 @@ void P::UDP::threadFunc() {
 				case Code::Success: {
 					if (pack.type() == Type::Ping) {
 						Message::Ping ping;
-						if (pack(ping, packet.size() - sizeof(NetHeader))) {
+						if (pack(ping, static_cast< uint32_t >(packet.size()) - sizeof(NetHeader))) {
 							if (m_feedback.ping) {
 								m_feedback.ping(endpoint, ping);
 							}

src/Socket.cpp
@@ -40,16 +40,16 @@ Socket::Socket(const Type type) {
 		case Type::Local:
 #ifdef OS_WINDOWS
 			// https://github.com/microsoft/WSL/issues/5272
-			m_fd = socket(PF_UNIX, SOCK_STREAM, 0);
+			m_fd = static_cast< int32_t >(socket(PF_UNIX, SOCK_STREAM, 0));
 #else
 			m_fd = socket(PF_LOCAL, SOCK_DGRAM, 0);
 #endif
 			break;
 		case Type::TCP:
-			m_fd = socket(PF_INET6, SOCK_STREAM, IPPROTO_TCP);
+			m_fd = static_cast< int32_t >(socket(PF_INET6, SOCK_STREAM, IPPROTO_TCP));
 			break;
 		case Type::UDP:
-			m_fd = socket(PF_INET6, SOCK_DGRAM, IPPROTO_UDP);
+			m_fd = static_cast< int32_t >(socket(PF_INET6, SOCK_DGRAM, IPPROTO_UDP));
 			break;
 		default:
 			m_fd = invalidFD;
@@ -179,7 +179,7 @@ Pair Socket::localPair() {
 
 	DeleteFile(addr.sun_path);
 
-	auto second = accept(listener.fd(), nullptr, nullptr);
+	auto second = static_cast< int32_t >(accept(listener.fd(), nullptr, nullptr));
 	if (!second) {
 		return {};
 	}

src/TCP.cpp
@@ -41,7 +41,7 @@ std::pair< int, int32_t > SocketTCP::accept(Endpoint &endpoint) {
 	socklen_t size = sizeof(addr);
 #endif
 	// Explicit return data type because socket handles are unsigned on Windows.
-	const int8_t fd = ::accept(m_fd, reinterpret_cast< sockaddr * >(&addr), &size);
+	const auto fd = static_cast< int32_t >(::accept(m_fd, reinterpret_cast< sockaddr * >(&addr), &size));
 	if (fd == invalidFD) {
 		return { osError(), invalidFD };
 	}

src/UDP.cpp
@@ -10,10 +10,17 @@
 
 #ifdef OS_WINDOWS
 #	include <WS2tcpip.h>
+
+#	define CAST_BUF(var) reinterpret_cast< char * >(var)
+#	define CAST_BUF_CONST(var) reinterpret_cast< const char * >(var)
+#	define CAST_SIZE(var) static_cast< int >(var)
 #else
 #	include <netinet/in.h>
 #	include <sys/socket.h>
 #endif
+
+#define CAST_SOCKADDR(var) reinterpret_cast< sockaddr * >(var)
+#define CAST_SOCKADDR_CONST(var) reinterpret_cast< const sockaddr * >(var)
 
 using namespace mumble;
 
@@ -23,12 +30,12 @@ SocketUDP::SocketUDP() : Socket(Type::UDP) {
 Code SocketUDP::read(Endpoint &endpoint, BufRef &buf) {
 	sockaddr_in6 addr;
 #ifdef OS_WINDOWS
-	int addrsize   = sizeof(addr);
-	const auto ret = recvfrom(m_fd, reinterpret_cast< char * >(buf.data()), buf.size(), 0,
-							  reinterpret_cast< sockaddr * >(&addr), &addrsize);
+	auto addrsize  = static_cast< int >(sizeof(addr));
+	const auto ret = recvfrom(m_fd, CAST_BUF(buf.data()), CAST_SIZE(buf.size()), 0,
+							  CAST_SOCKADDR(&addr), &addrsize);
 #else
 	socklen_t addrsize = sizeof(addr);
-	const auto ret     = recvfrom(m_fd, buf.data(), buf.size(), 0, reinterpret_cast< sockaddr     *>(&addr), &addrsize);
+	const auto ret     = recvfrom(m_fd, buf.data(), buf.size(), 0, CAST_SOCKADDR(&addr), &addrsize);
 #endif
 	if (ret <= 0) {
 		return osErrorToCode(osError());
@@ -47,10 +54,10 @@ Code SocketUDP::write(const Endpoint &endpoint, const BufRefConst buf) {
 	endpoint.ip.toSockAddr(addr);
 	addr.sin6_port = Endian::toNetwork(endpoint.port);
 #ifdef OS_WINDOWS
-	const auto ret = sendto(m_fd, reinterpret_cast< const char * >(buf.data()), buf.size(), 0,
-							reinterpret_cast< sockaddr * >(&addr), sizeof(addr));
+	const auto ret = sendto(m_fd, CAST_BUF_CONST(buf.data()), CAST_SIZE(buf.size()), 0,
+							CAST_SOCKADDR_CONST(&addr), sizeof(addr));
 #else
-	const auto ret     = sendto(m_fd, buf.data(), buf.size(), 0, reinterpret_cast< sockaddr     *>(&addr), sizeof(addr));
+	const auto ret     = sendto(m_fd, buf.data(), buf.size(), 0, CAST_SOCKADDR_CONST(&addr), sizeof(addr));
 #endif
 	if (ret <= 0) {
 		return osErrorToCode(osError());