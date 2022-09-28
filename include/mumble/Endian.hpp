8b1ce3117c325497e1de86c74ad56d12cc60dcdf
Davide Beatrici <git@davidebeatrici.dev>
2022-09-28 08:34:56 +0200

    Endian: Make header-only
    This allows the compiler to fully optimize the call(s) and result(s).
    When the library is compiled as big endian toNetwork() and toHost() are no-op.
    
    Hiding the implementation didn't make sense, as it's only made of intrinsics.
    
    Unfortunately the "_byteswap_*" intrinsics are not compatible with "constexpr":
    
    error C3615: constexpr function 'mumble::Endian::swap' cannot result in a constant expression
    note: failure was caused by call of undefined function or one not declared 'constexpr'

--- include/mumble/Endian.hpp
+++ include/mumble/Endian.hpp
@@ -10,26 +10,59 @@
 
 #include <cstdint>
 
-namespace mumble {
-class MUMBLE_EXPORT Endian {
-public:
-	Endian();
-	virtual ~Endian() = delete;
+#ifdef MUMBLE_COMPILER_MSVC
+#	include <cstdlib>
+#endif
 
+namespace mumble {
+class Endian {
+public:
 	static constexpr bool isBig() { return SingleSplit64(0x0102030405060708).split[0] == 1; }
 	static constexpr bool isLittle() { return SingleSplit64(0x0102030405060708).split[0] == 8; }
+#ifdef MUMBLE_COMPILER_MSVC
+	static uint16_t swap(const uint16_t value) {
+		return _byteswap_ushort(value);
+#else
+	static constexpr uint16_t swap(const uint16_t value) {
+		return __builtin_bswap16(value);
+#endif
+	}
+#ifdef MUMBLE_COMPILER_MSVC
+	static uint32_t swap(const uint32_t value) {
+		return _byteswap_ulong(value);
+#else
+	static constexpr uint32_t swap(const uint32_t value) {
+		return __builtin_bswap32(value);
+#endif
+	}
+#ifdef MUMBLE_COMPILER_MSVC
+	static uint64_t swap(const uint64_t value) {
+		return _byteswap_uint64(value);
+#else
+	static constexpr uint64_t swap(const uint64_t value) {
+		return __builtin_bswap64(value);
+#endif
+	}
 
-	static uint16_t swap(const uint16_t value);
-	static uint32_t swap(const uint32_t value);
-	static uint64_t swap(const uint64_t value);
+	static constexpr uint16_t toNetwork(const uint16_t value) {
+		return isBig() ? value : swap(value);
+	}
+	static constexpr uint32_t toNetwork(const uint32_t value) {
+		return isBig() ? value : swap(value);
+	}
+	static constexpr uint64_t toNetwork(const uint64_t value) {
+		return isBig() ? value : swap(value);
+	}
 
-	static uint16_t toNetwork(const uint16_t value) { return isBig() ? value : swap(value); }
-	static uint32_t toNetwork(const uint32_t value) { return isBig() ? value : swap(value); }
-	static uint64_t toNetwork(const uint64_t value) { return isBig() ? value : swap(value); }
-
-	static uint16_t toHost(const uint16_t value) { return isBig() ? value : swap(value); }
-	static uint32_t toHost(const uint32_t value) { return isBig() ? value : swap(value); }
-	static uint64_t toHost(const uint64_t value) { return isBig() ? value : swap(value); }
+	static constexpr uint16_t toHost(const uint16_t value) {
+		return isBig() ? value : swap(value);
+	}
+	static constexpr uint32_t toHost(const uint32_t value) {
+		return isBig() ? value : swap(value);
+	}
+	static constexpr uint64_t toHost(const uint64_t value) {
+		return isBig() ? value : swap(value);
+	}
 
 private:
 	union SingleSplit64 {
@@ -38,9 +71,6 @@ private:
 
 		constexpr SingleSplit64(const uint64_t value) : single(value) {}
 	};
-
-	Endian(const Endian &)                    = delete;
-	virtual Endian &operator=(const Endian &) = delete;
 };
 } // namespace mumble
 
