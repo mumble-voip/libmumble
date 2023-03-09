a3b5df8f7d2006d531bf19bc0c69bab19c32adf9
Robert Adam <dev@robert-adam.de>
2023-03-09 20:21:25 +0100

    Add script to format entire source tree

---
+++ scripts/runClangFormat.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+#
+# Copyright 2020-2022 The Mumble Developers. All rights reserved.
+# Use of this source code is governed by a BSD-style license
+# that can be found in the LICENSE file at the root of the
+# Mumble source tree or at <https://www.mumble.info/LICENSE>.
+
+git ls-files --cached --modified --others -z -- \
+    ':^3rdparty/' ':^build*' '**/*'.cpp '**/*'.c '**/*'.hpp '**/*'.h '**/*'.cxx '**/*'.cc  |
+	xargs -0 -r -- clang-format --style=file -i
