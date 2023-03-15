82ef58bfe1851f60bc713d2148eb8c8a2c68c784
Robert Adam <dev@robert-adam.de>
2023-03-15 20:03:18 +0100

    Remove unneeded .git extension

--- cmake/setup_dependencies.cmake
+++ cmake/setup_dependencies.cmake
@@ -17,21 +17,21 @@ FetchContent_Declare(
 )
 FetchContent_Declare(
 	quickpool
-	GIT_REPOSITORY https://github.com/tnagler/quickpool.git
+	GIT_REPOSITORY https://github.com/tnagler/quickpool
 	# The latest release currently still has issues that make it unusable for us
 	GIT_TAG        ddc415bec1fc624e1c6b21c1b47063ca2eef84de
 	GIT_SHALLOW    ON
 )
 FetchContent_Declare(
 	wepoll
-	GIT_REPOSITORY https://github.com/piscisaureus/wepoll.git
+	GIT_REPOSITORY https://github.com/piscisaureus/wepoll
 	GIT_TAG        v1.5.8
 	GIT_SHALLOW    ON
 	PATCH_COMMAND  "${CMAKE_COMMAND}" -E copy "${PROJECT_SOURCE_DIR}/cmake/wepoll_cmakelists.txt" "./CMakeLists.txt"
 )
 FetchContent_Declare(
 	cmake_compiler_flags
-	GIT_REPOSITORY https://github.com/Krzmbrzl/cmake-compiler-flags.git
+	GIT_REPOSITORY https://github.com/Krzmbrzl/cmake-compiler-flags
 	GIT_TAG        v2.0.0
 	GIT_SHALLOW    ON
 )
